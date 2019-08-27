af0e22c0b2afc433fb59e3fecc1b976fdaf64c86
Jon Pecson <jonpecson.io@gmail.com>
2019-08-28 05:06:48 +0800

    refactor(transfer-transaction.ts): remove unused code

--- src/pages/@core/transactions/transaction-list-partials/transfer-transaction/transfer-transaction.ts
+++ src/pages/@core/transactions/transaction-list-partials/transfer-transaction/transfer-transaction.ts
@@ -34,47 +34,16 @@ export class TransferTransactionComponent {
 
   ngOnInit() {
     this.getMosaicInfo();
-    console.log("SIRIUS CHAIN WALLET: TransferTransactionComponent -> ngOnInit -> this.status", this.status)
-
   }
 
   async getMosaicInfo() {
-
-    // console.log('LOG: TransferTransactionComponent -> getMosaicInfo -> this.mosaics', this.mosaics);
-
-    // console.log('LOG: TransferTransactionComponent -> getMosaicInfo -> this.tx', this.tx);
-
-    // console.log('LOG: TransferTransactionComponent -> getMosaicInfo -> this.tx', this.tx.mosaics[0].id.id.toHex());
-
-
     this.MOSAIC_INFO = this.mosaics.find(m => {
       return m.hex == this.tx.mosaics[0].id.id.toHex();
     });
 
     this.MOSAIC_INFO.amount = this.tx.mosaics[0].amount.compact();
-
-    console.log('LOG: TransferTransactionComponent -> getMosaicInfo -> MOSAIC_INFO', this.MOSAIC_INFO);
-
-    // this.MOSAIC_INFO = this.mosaicsProvider.getMosaicInfo(this.tx.mosaics[0]);
-
-
     this.LOGO = this.utils.getLogo(this.MOSAIC_INFO);
     this.STATUS = this.status;
-
-    // this.AMOUNT = this.mosaicsProvider.getRelativeAmount1(this.tx.mosaics[0].amount.compact(), this.MOSAIC_INFO.divisibility)
-
     this.AMOUNT = this.mosaicsProvider.getRelativeAmount1(this.MOSAIC_INFO.amount, this.MOSAIC_INFO.divisibility)
-
-    // this.array.push(this.tx.mosaics[0])
-    // await this.mosaicsProvider.getOwnedMosaic(this.array).then(valores => {
-    //   valores.forEach(element => {
-    //     this.MOSAIC_INFO = element;
-    //     this.AMOUNT = element.amount;
-    //     if(this.AMOUNT){
-    //       this.hiden = true;
-    //     }
-    //   });
-    
-    // });
   }
 }
