8dfce33a7b215279e222dce7db549bfb57f82b70
Jon Pecson <jonpecson.io@gmail.com>
2019-09-30 21:01:57 +0800

    fix: nis wallet add UI issue (iPad)

--- src/pages/@core/services-modules/swap/import-wallet/import-wallet.ts
+++ src/pages/@core/services-modules/swap/import-wallet/import-wallet.ts
@@ -34,6 +34,8 @@ export class ImportWalletPage {
   walletColor: string = "wallet-1";
   walletName: string = "Primary";
 
+  tablet: boolean = false;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -73,6 +75,10 @@ export class ImportWalletPage {
   }
 
   init() {
+    if (window.screen.width >= 768) { // 768px portrait
+      this.tablet = true;
+    }
+    
     this.formGroup = this.formBuilder.group({
       name: ['', [Validators.minLength(3), Validators.required]],
       privateKey: ['', [Validators.minLength(3), Validators.required]]
