887e47d52ce4143d2f629fccc384aff773a2d083
rperaza <roimerj63@gmail.com>
2019-11-20 21:17:52 -0500

    Show transaction partials

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,34 +2,35 @@ import { NetworkType } from "tsjs-xpx-chain-sdk";
 import { NetworkTypes, ServerConfig } from "nem-library";
 
 export const AppConfig = {
-    app: {
-      environment: 'dev'
-    },
-    timeOutTransactionNis1: 20000,
-    sirius: {
-      httpNodeUrl: "https://bctestnet1.brimstone.xpxsirius.io",
-      wsNodeUrl: "ws://bctestnet1.brimstone.xpxsirius.io:3000",
-      networkType: NetworkType.TEST_NET,
-      networkGenerationHash: '56D112C98F7A7E34D1AEDC4BD01BC06CA2276DD546A93E36690B785E82439CA9'
-    },
-    xpxHexId: '13bfc518e40549d7',
-    mosaicXpxInfo: {
-      name: 'prx.xpx',
-      coin: 'XPX',
-      id: '13bfc518e40549d7',
-      mosaicIdUint64: [3825551831, 331334936],
-      namespaceIdUint64: [2434186742, 3220914849],
-      namespaceId: 'bffb42a19116bdf6',
-      divisibility: 6
-    },
-    swap:{
-      url: 'https://bctestnetswap.xpxsirius.io:7890',
-      urlExplorer: 'http://testnet-explorer.nemtool.com/#/s_tx?hash=',
-      networkType: NetworkTypes.TEST_NET,
-      burnAddress: 'TBF4LAZUEJMBIOC6J24D6ZGGXE5W775TX555CTTN',
+  app: {
+    environment: 'dev'
+  },
+  timeOutTransactionNis1: 20000,
+  sirius: {
+    httpNodeUrl: "https://bctestnet1.brimstone.xpxsirius.io",
+    wsNodeUrl: "ws://bctestnet1.brimstone.xpxsirius.io:3000",
+    networkType: NetworkType.TEST_NET,
+    networkGenerationHash: '56D112C98F7A7E34D1AEDC4BD01BC06CA2276DD546A93E36690B785E82439CA9'
+  },
+  xpxHexId: '13bfc518e40549d7',
+  mosaicXpxInfo: {
+    name: 'prx.xpx',
+    coin: 'XPX',
+    id: '13bfc518e40549d7',
+    mosaicIdUint64: [3825551831, 331334936],
+    namespaceIdUint64: [2434186742, 3220914849],
+    namespaceId: 'bffb42a19116bdf6',
+    divisibility: 6
+  },
+  swap: {
+    url: 'https://bctestnetswap.xpxsirius.io:7890',
+    urlExplorer: 'http://testnet-explorer.nemtool.com/#/s_tx?hash=',
+    networkType: NetworkTypes.TEST_NET,
+    burnAddress: 'TBF4LAZUEJMBIOC6J24D6ZGGXE5W775TX555CTTN',
+    addressAccountMultisig: 'VAWOEOWTABXR7O3ZAK2XNA5GIBNE6PZIXDAFDWBU',
+    addressAccountSimple: 'VCWLIYQPQAJSYWMWL5BHUCA3VOWVOXZ3WTNJPTUJ',
       nodes: [
-        { protocol: "https", domain: "bctestnetswap.xpxsirius.io", port: 7890 } as ServerConfig
-      ],
-    }
-  };
-  
+      { protocol: "https", domain: "bctestnetswap.xpxsirius.io", port: 7890 } as ServerConfig
+    ],
+  }
+};
