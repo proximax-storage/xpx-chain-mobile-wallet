8df8b26d82d99a7996cfe71fd4a89874ee0a2fcb
hachibi1 <hachibi1515@gmail.com>
2019-11-28 11:57:45 -0500

    icon dashboard ans translate

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,10 +1,12 @@
 
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, Events, Tabs, ModalController, FabContainer } from 'ionic-angular';
+import { IonicPage, Events, Tabs, ModalController, ActionSheetController } from 'ionic-angular';
 
 import { HomePage } from '../home/home';
 import { UtilitiesProvider } from '../../providers/utilities/utilities';
 import { PostsProvider } from '../../providers/posts/posts';
+import { AlertProvider } from '../../providers/alert/alert';
+import { WalletProvider } from '../../providers/wallet/wallet';
 
 @IonicPage()
 @Component({
@@ -22,22 +24,23 @@ export class TabsPage {
   notificationCount: number = 0; // To do get read count from API
 
   @ViewChild(Tabs) public tabs: Tabs;
-
+  account: any = [];
   constructor(
     public events: Events,
     private utils: UtilitiesProvider,
     private modalCtrl: ModalController,
     private articles: PostsProvider,
+    public actionsheetCtrl: ActionSheetController,
+    private alertProvider: AlertProvider,
+    private walletProvider: WalletProvider
   ) {
     this.articles.getUnreadCount().then(count => {
       // console.log("Unread count", count);
       this.notificationCount = count;
+      this.account = this.walletProvider.selectesAccount;
     })
   }
 
-  closeFab(fab: FabContainer){
-    fab.close();
-}
   ionViewWillEnter() {
     this.utils.setHardwareBack();
 
@@ -71,5 +74,43 @@ export class TabsPage {
   clearNotification() {
     console.log("Clearing notification count");
     this.notificationCount = 0;
+  }
+
+  openMenu() {
+    if (this.account != undefined || this.account != null){
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'Options',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Send',
+          icon: 'custom-send',
+          handler: () => {
+            console.log('Share clicked');
+            this.gotoSend();
+          }
+        },
+        {
+          text: 'Receive',
+          icon:  'custom-receive',
+          handler: () => {
+            console.log('Delete clicked');
+            this.gotoReceive();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  } else {
+    this.alertProvider.showMessage('No possee cuentas aun')
+  }
   }
 }
