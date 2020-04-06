53b8d6ee782b6db7cb2779b6a8c9f0eb50916d3e
hachibi1 <49165887+hachibi1@users.noreply.github.com>
2020-04-06 05:37:36 -0500

    fuction scan

--- src/pages/services/services.ts
+++ src/pages/services/services.ts
@@ -3,7 +3,7 @@ import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angu
 import { UtilitiesProvider } from '../../providers/utilities/utilities';
 import { AlertProvider } from '../../providers/alert/alert';
 import { TranslateService } from '@ngx-translate/core';
-// import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import { ProximaxProvider } from '../../providers/proximax/proximax';
 import { Storage } from "@ionic/storage";
 import { WalletProvider } from '../../providers/wallet/wallet';
@@ -30,7 +30,7 @@ export class ServicesPage {
     private modalCtrl: ModalController,
     private alertProvider: AlertProvider,
     private translateService: TranslateService,
-    // private barcodeScanner: BarcodeScanner,
+    private barcodeScanner: BarcodeScanner,
     private proximaxProvider: ProximaxProvider,
     private storage: Storage,
     private walletProvider: WalletProvider, ) {
@@ -76,37 +76,37 @@ export class ServicesPage {
     if (this.account != null) {
 
       // FUNCION SCAN QUE CAPTURA LOS VALORES DEL QR Y LOS DESERIALIZA
-      // this.barcodeScanner
-      //   .scan()
-      //   .then(barcodeData => {
-      //     barcodeData.format = "QR_CODE";
+      this.barcodeScanner
+        .scan()
+        .then(barcodeData => {
+          barcodeData.format = "QR_CODE";
 
-      //     if (barcodeData.cancelled === true) {
-      //       console.log('no tiene datos el scan');
-      //     } else {
-      //       // aqui va la validacion 
-      //       const dataFormat = this.proximaxProvider.unSerialize(barcodeData.text)
-      //       if (dataFormat && dataFormat[0].mosaicGift && dataFormat[0].pkGift) {
-      //         this.gotoGift('GiftCardsPage', dataFormat)
-      //       } else {
-      //         this.alertProvider.showMessage(this.translateService.instant("SERVICES.GIFT_CARD.TRANSFER.ERROR"));
-      //       }
-      //     }
+          if (barcodeData.cancelled === true) {
+            console.log('no tiene datos el scan');
+          } else {
+            // aqui va la validacion 
+            const dataFormat = this.proximaxProvider.unSerialize(barcodeData.text)
+            if (dataFormat && dataFormat[0].mosaicGift && dataFormat[0].pkGift) {
+              this.gotoGift('GiftCardsPage', dataFormat)
+            } else {
+              this.alertProvider.showMessage(this.translateService.instant("SERVICES.GIFT_CARD.TRANSFER.ERROR"));
+            }
+          }
 
-      //   })
-      //   .catch(err => {
-      //     if (err.toString().indexOf(
-      //       this.translateService.instant("WALLETS.SEND.ERROR.CAMERA1")) >= 0) {
-      //       let message = this.translateService.instant("WALLETS.SEND.ERROR.CAMERA2");
-      //       this.alertProvider.showMessage(message);
-      //     }
-      //   });
+        })
+        .catch(err => {
+          if (err.toString().indexOf(
+            this.translateService.instant("WALLETS.SEND.ERROR.CAMERA1")) >= 0) {
+            let message = this.translateService.instant("WALLETS.SEND.ERROR.CAMERA2");
+            this.alertProvider.showMessage(message);
+          }
+        });
 
 
       // DATA DE PRUEBA BINARIO CERIALIZADO DE LA GIFT CARD
 
       // misaics no transferable
-      const dataHex = '0000000000000001382CF8EF60DE40E39925595FC710DE81E2329F2991A4CA4D889C7F6FFE949D7C4FF17E357254D45130CBFA68'
+      /* const dataHex = '00000000000000013035777FC55F5D6FD757C04F0CDB10D9A9766EC58264D19D316DF79EAB97D6C24FF17E357254D45130374A48594436'
  
        // misaics transferable
        // const dataHex = '000000E8D4A51000942110B5FF15C06141A14322E7A3054D5B1227215B7836224F106471C1AAF2ED13BFC518E40549D7316378313539383735363734333435363738363533'
@@ -117,7 +117,7 @@ export class ServicesPage {
          } else {
            this.alertProvider.showMessage(this.translateService.instant("SERVICES.GIFT_CARD.TRANSFER.ERROR"));
          } 
- 
+ */
       // FIN DATA DE PRUEBA BINARIO CERIALIZADO DE LA GIFT CARD
 
     } else {
